6b48fca956308d8d547d39a69f30cf137ea140e7
Błażej Biesiada <blazej@biesiada.net.pl>
2012-02-22 10:38:36 +0100

    Added macros to fix Reachability on OS X (there's no WWAN).

--- External/Reachability/Reachability.m
+++ External/Reachability/Reachability.m
@@ -132,7 +132,9 @@ static NSString *reachabilityFlags_(SCNetworkReachabilityFlags flags) {
 #else
 	// Compile out the v3.0 features for v2.2.1 deployment.
     return [NSString stringWithFormat:@"Reachability Flags: %c%c %c%c%c%c%c%c",
+            #if TARGET_OS_IPHONE
 			(flags & kSCNetworkReachabilityFlagsIsWWAN)               ? 'W' : '-',
+            #endif
 			(flags & kSCNetworkReachabilityFlagsReachable)            ? 'R' : '-',
 			
 			(flags & kSCNetworkReachabilityFlagsConnectionRequired)   ? 'c' : '-',
@@ -439,7 +441,9 @@ const SCNetworkReachabilityFlags kConnectionDown =  kSCNetworkReachabilityFlagsC
 		// WWAN Connection required: Reachability Flag Status: WR ct-----
 		//
 		// Test Value: Reachability Flag Status: WR xxxxxxx
+        #if TARGET_OS_IPHONE 
 		if (flags & kSCNetworkReachabilityFlagsIsWWAN) { return kReachableViaWWAN; }
+        #endif 
 		
 		// Clear moot bits.
 		flags &= ~kSCNetworkReachabilityFlagsReachable;
